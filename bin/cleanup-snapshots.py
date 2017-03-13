8facc8062238f705f2706b70267f8a94c5948a17
Dan Davis <daniel.davis@nih.gov>
2017-03-13 14:51:02 -0400

    Improve the cleanup-snapshots so it can do work
    - Has a list operation where it lists registered "image" snapshots
    - Has a remove operation where it removes those snapshots that are not
      registered to an image - never tried it.

--- bin/cleanup-snapshots.py
+++ bin/cleanup-snapshots.py
@@ -1,11 +1,15 @@
 import boto3
 import re
+import argparse
+from getpass import getpass
 
 
 def snapshots_matching_description(ec2, owner, description_expr):
+    snapshots = set()
     for snapshot in ec2.snapshots.filter(OwnerIds=[str(owner)]):
         if re.match(description_expr, snapshot.description):
-            yield snapshot
+            snapshots.add(snapshot.id)
+    return snapshots
 
 
 def snapshots_registered_to_an_image(ec2, owner):
@@ -17,33 +21,47 @@ def snapshots_registered_to_an_image(ec2, owner):
     return snapshots
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('operation', choices=('list', 'remove'))
+    parser.add_argument('--owner', metavar='OWNER_ID', default=765460880451)
+    parser.add_argument('--region', metavar='REGION_NAME', default='us-east-1')
+    return parser.parse_args()
+
+
 def main():
 
-    owner = 765460880451
-    region = "us-east-1"
+    opts = parse_args()
+    owner = opts.owner
+    region = opts.region
     ec2 = boto3.resource("ec2", region_name=region)
 
-    print("\nSnapshots currently registered to an image:")
     registered_snapshot_ids = snapshots_registered_to_an_image(ec2, owner)
-    for snapshot_id in registered_snapshot_ids:
-        print("   %s" % snapshot_id)
-    imported_snapshot_ids = set()
-
-    print("\nSnapshots created by AWS-VMImport:")
     aws_vmimport_snapshots = snapshots_matching_description(ec2, owner, 'Created by AWS-VMImport service')
-    for snapshot in aws_vmimport_snapshots:
-        imported_snapshot_ids.add(snapshot.id)
-        print("   %s" % snapshot.id)
-
-    print("\nSnapshots created by copying:")
     copied_for_ami_snapshots = snapshots_matching_description(ec2, owner, 'Copied for DestinationAmi')
-    for snapshot in copied_for_ami_snapshots:
-        imported_snapshot_ids.add(snapshot.id)
-        print("   %s" % snapshot.id)
+    imported_snapshot_ids = aws_vmimport_snapshots.union(copied_for_ami_snapshots)
+    unregistered_snapshot_ids = imported_snapshot_ids.difference(registered_snapshot_ids)
 
-    print("\nSnapshots that probably can go:")
-    for snapshot_id in imported_snapshot_ids.difference(registered_snapshot_ids):
-        print("   %s" % snapshot_id)
+    if opts.operation == 'list':
+        print("\nRegistered image snapshots:")
+        for snapshot_id in registered_snapshot_ids:
+            print("   %s" % snapshot_id)
+
+        print("\nUnregistered image snapshots:")
+        for snapshot_id in unregistered_snapshot_ids:
+            print("   %s" % snapshot_id)
+
+    elif opts.operation == 'remove':
+
+        print("\nUnregistered image snapshots:")
+        for snapshot_id in unregistered_snapshot_ids:
+            print("   %s" % snapshot_id)
+        remove = getpass("\nRemove snapshots above? [yN] ")
+        if remove == 'y' or remove == 'Y':
+            print("Removing snapshots...")
+            for snapshot_id in unregistered_snapshot_ids:
+                snapshot = ec2.Snapshot(snapshot_id)
+                snapshot.delete()
 
 
 if __name__ == '__main__':
